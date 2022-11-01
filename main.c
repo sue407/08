ab3bcae9be31736f5fd215ad152d34cf427fdb86
sue407 <sue407407@gmail.com>
2022-11-01 16:45:44 +0900

    08 (3)-1
    11/1 (3)-1

--- main.c
+++ main.c
@@ -5,22 +5,19 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i, average;
-	int sum;
+	int i;
+	int score[SIZE];
 	int grade[SIZE];
-	
-	sum = 0;
 	
 	
 	for(i=0; i<SIZE; i++)
-	{
-		printf("학생 성적을 입력하세요 : ");
-	    scanf("%d", &grade[i]);
-	    sum += grade[i];
+	 grade[i] = rand() % 100;
 
-	}
-	average = sum / SIZE;
-	printf("성적평균 :  %d\n", average);
+	for(i=0; i<SIZE; i++)
+	 score[i] = grade[i];
+
+	for(i=0; i<SIZE; i++)
+	printf("grade[%d] = %d\n score[%d] = %d\n", i, grade[i], i, score[i]);
 	 
 	return 0;
 }
